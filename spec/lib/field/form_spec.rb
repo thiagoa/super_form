bed7e2a114d203ae3e8b0969fa956e7dd49c9dba
Thiago A. Silva <thiago@mixinternet.com.br>
2014-02-13 21:20:10 -0300

    Same testing approach for form_spec

--- spec/lib/field/form_spec.rb
+++ spec/lib/field/form_spec.rb
@@ -1,35 +1,37 @@
 require 'spec_helper'
 require 'super_form'
 
-class ParentForm
-  include SuperForm
-end
-
-class ChildForm
-  include SuperForm
-
-  field :empty, Field::Text, presence: true
-end
-
 describe Field::Form do
   context 'when creating a form field isolatedly' do
-    before do
-      @field = Field::Form.new(:fake)
-      @field.form_class = ChildForm
-    end
+    it 'assigns the child form to the field attribute' do
+      child_form = Class.new { include SuperForm }
 
-    it 'stores the child form as the field attribute' do
-      expect(@field.attribute).to eq ChildForm
+      field = Field::Form.new(:fake)
+      field.form_class = child_form
+
+      expect(field.attribute).to eq child_form
     end
   end
 
   context 'when the form field has a container' do
     before do
-      field = Field::Form.new(:child_form)
-      field.form_class = ChildForm
-      field.setup_container(ParentForm)
+      @child_form = Class.new do
+        include SuperForm
 
-      @parent_form = ParentForm.new
+        field :name, Field::Text, presence: true
+
+        def self.model_name
+          ActiveModel::Name.new(self, nil, 'anonymous')
+        end
+      end
+
+      parent_form = Class.new { include SuperForm }
+
+      field = Field::Form.new(:child_form)
+      field.form_class = @child_form
+      field.setup_container(parent_form)
+
+      @parent_form = parent_form.new
     end
 
     describe '#inject_attributes' do
@@ -39,7 +41,7 @@ describe Field::Form do
       end
 
       it 'returns an instance of child form as the default value' do
-        expect(@parent_form.child_form).to be_instance_of(ChildForm)
+        expect(@parent_form.child_form).to be_instance_of(@child_form)
       end
     end
 
@@ -53,7 +55,9 @@ describe Field::Form do
 
   describe '#[]' do
     before do
-      proxy = Field::Form[ChildForm]
+      @child_form = Class.new { include SuperForm }
+      proxy = Field::Form[@child_form]
+
       @field = proxy.factory(:name)
     end
 
@@ -62,7 +66,7 @@ describe Field::Form do
     end
 
     it 'stores the child form into the form_class attribute' do
-      expect(@field.form_class).to eq ChildForm
+      expect(@field.form_class).to eq @child_form
     end
   end
 end
